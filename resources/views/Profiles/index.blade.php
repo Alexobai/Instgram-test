43c74b802f08e4b7a6c25fc96f1d310fc38c106b
unknown <shhuang@clarkson.edu>
2019-10-09 11:37:10 -0400

    delete customized stuff

--- resources/views/Profiles/index.blade.php
+++ resources/views/Profiles/index.blade.php
@@ -3,51 +3,46 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3 p-5"> <!--p is pading all the way round-->
-            <img src="{{$user->profile->profileImage()}}" class = "rounded-circle w-100">
+        <div class="col-3 p-5">
+            <img src="{{ $user->profile->profileImage() }}" class="rounded-circle w-100">
         </div>
-        <div class="col-9 pt-5 pl-5">
-            <div class = "d-flex justify-content-between align-items-baseline">
-               <div class = "d-flex align-item-center pb-3">
-                    <div class = "h-4" >{{$user->username}}</div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">{{ $user->username }}</div>
 
-                    <follow-button user-id="{{$user->id}}" follows="{{$follows}}"></follow-button> <!--ml-4 = margin left 4 -->
-               </div>
+                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
+                </div>
 
-                @can('update',$user->profile)
-                    <a href="/p/create">Add a new post</a> 
+                @can('update', $user->profile)
+                    <a href="/p/create">Add New Post</a>
                 @endcan
-                 
-            </div> <!-- echo out sth in view method -->
-            @can('update',$user->profile)
-                <a href="/profile/{{$user->id}}/edit">Edit Profile Link</a>
-            @endcan
-            <div class = "d-flex">
-                <div class = "pr-5"><strong>{{$postCount}}</strong> posts</div>
-                <div class = "pr-5"><strong>{{$followersCount}}</strong> followers</div>
-                <div class = "pr-5"><strong>{{$followingCount}}</strong> following</div>
+
             </div>
-            <div class = "pt-4 font-weight-bold">{{$user->profile->title}}</div>
-            <div>{{$user->profile->description}}</div>
+
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
+
+            <div class="d-flex">
+                <div class="pr-5"><strong>{{ $postCount }}</strong> posts</div>
+                <div class="pr-5"><strong>{{ $followersCount }}</strong> followers</div>
+                <div class="pr-5"><strong>{{ $followingCount }}</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
             <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
-    <div class="row pt-4">
+    <div class="row pt-5">
         @foreach($user->posts as $post)
-        <div class="col-4 pb-4 ">
-            <a href="/p/{{$post->id}}">
-                <img src="/storage/{{ $post->image }}" class="w-100">
-            </a>
-        </div>
-        <div class="col-4">
-            <img src="http://steam.cryotank.net/wp-content/gallery/batmanarkhamknight/Batman-AK-05.png" class="w-100">
-        </div>
-        <div class="col-4">
-            <img src="https://patchbot.io/images/games/call_of_duty_black_ops_4_md.jpg" class = "w-100">
-        </div>
+            <div class="col-4 pb-4">
+                <a href="/p/{{ $post->id }}">
+                    <img src="/storage/{{ $post->image }}" class="w-100">
+                </a>
+            </div>
         @endforeach
-       
     </div>
 </div>
 @endsection
